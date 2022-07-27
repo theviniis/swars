0ddcaab20d2a829f2b01023688a1972b0fb9f5ff
Vinicius Costa <vinicius.costa@cwfirpo.com.br>
2022-07-27 17:44:34 -0300

    first commit

--- src/styles/globalStyles.jsx
+++ src/styles/globalStyles.jsx
@@ -1,6 +1,6 @@
 import { createGlobalStyle } from 'styled-components';
 
-export const GlobalStyles = createGlobalStyle`
+const GlobalStyles = createGlobalStyle`
   * {
     margin: 0;
     padding: 0;
@@ -33,3 +33,5 @@ export const GlobalStyles = createGlobalStyle`
   }
 
 `;
+
+export default GlobalStyles;
