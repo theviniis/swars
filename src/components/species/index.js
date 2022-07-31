3b84d9f22478c788fccc0da51108593c49cc8d9b
Vinicius Costa <vinicius.dsc95@gmail.com>
2022-07-31 00:26:56 -0300

    ajustes nas rotas, refatorado componente de lista de atributos, criado paginas worlds e species

--- src/components/species/index.js
+++ src/components/species/index.js
@@ -8,75 +8,72 @@ const Species = () => {
 
   return (
     <Page name='Species'>
-      <div className='species__list'>
-        {species.response
-          ? species.response.results.map((item) => (
-              <AttrList
-                key={item.name}
-                data={[
-                  {
-                    name: 'name',
-                    url: item.name,
-                  },
-                  {
-                    name: 'average_height',
-                    url: item.average_height,
-                  },
-                  {
-                    name: 'average_lifespan',
-                    url: item.average_lifespan,
-                  },
-                  {
-                    name: 'classification',
-                    url: item.classification,
-                  },
-                  {
-                    name: 'created',
-                    url: item.created,
-                  },
-                  {
-                    name: 'designation',
-                    url: item.designation,
-                  },
+      {species.response
+        ? species.response.results.map((item) => (
+            <AttrList
+              key={item.name}
+              data={[
+                {
+                  name: 'name',
+                  url: item.name,
+                },
+                {
+                  name: 'average_height',
+                  url: item.average_height,
+                },
+                {
+                  name: 'average_lifespan',
+                  url: item.average_lifespan,
+                },
+                {
+                  name: 'classification',
+                  url: item.classification,
+                },
+                {
+                  name: 'created',
+                  url: item.created,
+                },
+                {
+                  name: 'designation',
+                  url: item.designation,
+                },
 
-                  {
-                    name: 'edited',
-                    url: item.edited,
-                  },
-                  {
-                    name: 'eye_colors',
-                    url: item.eye_colors,
-                  },
-                  {
-                    name: 'films',
-                    url: item.films,
-                  },
-
-                  {
-                    name: 'hair_colors',
-                    url: item.hair_colors,
-                  },
-                  {
-                    name: 'homeworld',
-                    url: item.homeworld,
-                  },
-                  {
-                    name: 'language',
-                    url: item.language,
-                  },
-                  {
-                    name: 'people',
-                    url: item.people,
-                  },
-                  {
-                    name: 'skin_colors',
-                    url: item.skin_colors,
-                  },
-                ]}
-              />
-            ))
-          : null}
-      </div>
+                {
+                  name: 'edited',
+                  url: item.edited,
+                },
+                {
+                  name: 'eye_colors',
+                  url: item.eye_colors,
+                },
+                {
+                  name: 'hair_colors',
+                  url: item.hair_colors,
+                },
+                {
+                  name: 'homeworld',
+                  url: item.homeworld,
+                },
+                {
+                  name: 'language',
+                  url: item.language,
+                },
+                {
+                  name: 'skin_colors',
+                  url: item.skin_colors,
+                },
+                {
+                  name: 'films',
+                  url: item.films,
+                },
+                {
+                  name: 'people',
+                  url: item.people,
+                },
+              ]}
+            />
+          ))
+        : null}
     </Page>
   );
 };
