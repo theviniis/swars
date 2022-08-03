a1048fd4c846011751b69834ca2ed8f1d4729470
Vinicius Costa <vinicius.costa@cwfirpo.com.br>
2022-08-03 16:58:54 -0300

    adicionado versão do node package json

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import NavBar from 'components/Navbar/NavBar';
-import Feed from 'components/Feed/Feed';
-import 'styles/application.scss';
-import PeopleBar from 'components/PeopleBar';
-import Worlds from 'components/Worlds';
-import Species from 'components/Species';
-import NotFound from 'components/NotFound';
+import NavBar from './components/Navbar/NavBar';
+import Feed from './components/Feed/Feed';
+import './styles/application.scss';
+import PeopleBar from './components/PeopleBar';
+import Worlds from './components/Worlds';
+import Species from './components/Species';
+import NotFound from './components/NotFound';
 
 function App() {
   return (
