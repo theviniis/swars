17097592fb11699b7a9c9e1723e67556a3279444
Vinicius Costa <vinicius.dsc95@gmail.com>
2022-08-03 01:12:31 -0300

    refatorado nav bar no mobile

--- src/components/navbar/NavBar.js
+++ src/components/navbar/NavBar.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import { ReactComponent as Logo } from '../../assets/logo.svg';
+import useMedia from '../../hooks/useMedia';
 import NavList from './NavList';
 import ProfileCard from './ProfileCard';
 
 const NavBar = () => {
+  const mobile = useMedia('(max-width: 600px)');
+  console.log(mobile);
   return (
     <>
-      <nav className={'nav'}>
+      <nav className={`nav ${mobile ? 'mobile' : ''}`}>
         <Logo className='logo' />
         <NavList />
         <ProfileCard />
