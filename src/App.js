4c814d335faab181ad5afaf2ed2fc2039e92fa6b
Vinicius Costa <vinicius.costa@cwfirpo.com.br>
2022-08-03 11:04:57 -0300

    criado rota para page 404

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import './styles/application.scss';
 import PeopleBar from './components/PeopleBar';
 import Worlds from './components/Worlds';
 import Species from './components/Species';
+import NotFound from './components/NotFound';
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
           <Route path='/' element={<Feed />} />
           <Route path='worlds' element={<Worlds />} />
           <Route path='species' element={<Species />} />
+          <Route path='*' element={<NotFound />} />
         </Routes>
         <PeopleBar />
       </div>
